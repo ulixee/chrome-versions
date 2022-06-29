6cccbeeeec4b9c5d877114f2a0d44bc3d57e9498
Blake Byrnes <blakebyrnes@gmail.com>
2022-06-29 16:42:28 -0400

    fix: try using native 7zip

--- scripts/windows/install7Zip.ps1
+++
@@ -1,6 +0,0 @@
-$dlurl = 'https://7-zip.org/' + (Invoke-WebRequest -Uri 'https://7-zip.org/' | Select-Object -ExpandProperty Links | Where-Object {($_.innerHTML -eq 'Download') -and ($_.href -like "a/*") -and ($_.href -like "*-x64.exe")} | Select-Object -First 1 | Select-Object -ExpandProperty href)
-		# above code from: https://perplexity.nl/windows-powershell/installing-or-updating-7-zip-using-powershell/
-$installerPath = Join-Path $env:TEMP (Split-Path $dlurl -Leaf)
-Invoke-WebRequest $dlurl -OutFile $installerPath
-Start-Process -FilePath $installerPath -Args "/S" -Verb RunAs -Wait
-Remove-Item $installerPath
